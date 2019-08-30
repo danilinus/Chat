6358868650ee70217863a13285034189f6fe17b8
Danila <danilagalkevich@gmail.com>
2019-08-30 23:00:06 +0300

    Update MainViewPager.java

--- ClientChat/app/src/main/java/com/jobs/clientchat/MainViewPager.java
+++ ClientChat/app/src/main/java/com/jobs/clientchat/MainViewPager.java
@@ -11,71 +11,61 @@ import java.lang.reflect.Field;
 
 public class MainViewPager extends ViewPager {
 
-    public MainViewPager(Context context) {
-        super(context);
-        postInitViewPager();
-    }
+	public MainViewPager(Context context) {
+		super(context);
+		postInitViewPager();
+	}
 
-    public MainViewPager(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        postInitViewPager();
-    }
+	public MainViewPager(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		postInitViewPager();
+	}
 
-    private MainViewPagerDuration mScroller = null;
+	private MainViewPagerDuration mScroller = null;
 
-    /**
-     * Override the Scroller instance with our own class so we can change the
-     * duration
-     */
-    private void postInitViewPager() {
-        try {
-            Field scroller = ViewPager.class.getDeclaredField("mScroller");
-            scroller.setAccessible(true);
-            Field interpolator = ViewPager.class.getDeclaredField("sInterpolator");
-            interpolator.setAccessible(true);
+	private void postInitViewPager() {
+		try {
+			Field scroller = ViewPager.class.getDeclaredField("mScroller");
+			scroller.setAccessible(true);
+			Field interpolator = ViewPager.class.getDeclaredField("sInterpolator");
+			interpolator.setAccessible(true);
 
-            mScroller = new MainViewPagerDuration(getContext(),
-                    (Interpolator) interpolator.get(null));
-            scroller.set(this, mScroller);
-        } catch (Exception e) {
-        }
-    }
+			mScroller = new MainViewPagerDuration(getContext(),
+					(Interpolator) interpolator.get(null));
+			scroller.set(this, mScroller);
+		} catch (Exception e) {
+		}
+	}
 
-    /**
-     * Set the factor by which the duration will change
-     */
-    public void setScrollDurationFactor(double scrollFactor) {
-        mScroller.setScrollDurationFactor(scrollFactor);
-    }
+	public void setScrollDurationFactor(double scrollFactor) {
+		mScroller.setScrollDurationFactor(scrollFactor);
+	}
 }
 
 class MainViewPagerDuration extends Scroller {
 
-    private double mScrollFactor = 1;
+	private double mScrollFactor = 1;
 
-    public MainViewPagerDuration(Context context) {
-        super(context);
-    }
+	public MainViewPagerDuration(Context context) {
+		super(context);
+	}
 
-    public MainViewPagerDuration(Context context, Interpolator interpolator) {
-        super(context, interpolator);
-    }
+	public MainViewPagerDuration(Context context, Interpolator interpolator) {
+		super(context, interpolator);
+	}
 
-    @SuppressLint("NewApi")
-    public MainViewPagerDuration(Context context, Interpolator interpolator, boolean flywheel) {
-        super(context, interpolator, flywheel);
-    }
+	@SuppressLint("NewApi")
+	public MainViewPagerDuration(Context context, Interpolator interpolator, boolean flywheel) {
+		super(context, interpolator, flywheel);
+	}
 
-    /**
-     * Set the factor by which the duration will change
-     */
-    public void setScrollDurationFactor(double scrollFactor) {
-        mScrollFactor = scrollFactor;
-    }
+	public void setScrollDurationFactor(double scrollFactor) {
+		mScrollFactor = scrollFactor;
+	}
 
-    @Override
-    public void startScroll(int startX, int startY, int dx, int dy, int duration) {
-        super.startScroll(startX, startY, dx, dy, (int) (duration * mScrollFactor));
-    }
+	@Override
+	public void startScroll(int startX, int startY, int dx, int dy, int duration) {
+		super.startScroll(startX, startY, dx, dy, (int) (duration * mScrollFactor));
+	}
 
 }
